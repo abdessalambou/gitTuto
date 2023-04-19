3e740f8a57677bef7f454d95eb77f42b4c0889b5
abdessalamboulayat <abdessalam6boulayat@gmail.com>
2023-04-19 23:49:54 +0000

    message commit

--- file1.py
+++ file1.py
@@ -19,3 +19,13 @@ def functionHello(name):
     return "Hello world"
 
 functionGo('abdessalam')
+
+def functionStop(name):
+    """
+    this function allow us to say good bye name
+    Parameters:
+            name (string): name of the user
+    Return:
+            good bye name
+    """
+    return "stop bye"
